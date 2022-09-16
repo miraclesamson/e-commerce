0d635cdb6bd122cac9a730453595710288169386
miraclesamson <106530260+miraclesamson@users.noreply.github.com>
2022-09-16 17:27:02 -0500

    Delete .history directory

--- .history/src/pages/Login_20220901105200.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import * as Yup from "yup";
-import { useFormik } from "formik";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-
-import { fireAuth } from "../firebase";
-
-export default Login;
--- .history/tailwind.config_20220904045324.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: [
-    "./components/**/*.{html,js}",
-    "./pages/**/*.{html,js}",
-    "./index.html",
-  ],
-  // ...
-};
